2dd0c5d00c8efe2d701ad758e5f9aaab05a3ead8
Budiprasetyo94 <budiprasetyo131294@gmail.com>
2017-12-17 17:38:41 +0700

    zo

--- kota.py
+++ kota.py
@@ -7,3 +7,14 @@ def option():
 	pilihan = int (input("masukan pilihan anda : "))
 	return pilihan
 	
+pilihan = True 
+while (pilihan<4) :
+	pilihan = option()
+	if (pilihan==1):
+		kotaasal=str(input("Masukan nama kota asal : "))
+		kotatujuan=str(input("Masukan nama kota tujuan : "))
+		jarak=int(input("masukan jarak antar kota : " ))
+		kecepatan=int(input("kecepatan rata-rata kendaraan : "))
+		waktutempuh=jarak/kecepatan
+		print("Waktu Yang Anda Tempuh : %.2f"%(waktutempuh),"Jam")
+		print("=====================================================\n")
